717c33bee7c2639208ac2f94a6275663072d806b
DashPunk <mordwalzer@gmail.com>
2021-06-22 15:51:20 +0200

    refactor: вынести изначальные карточки в отдельный файл

--- scripts/index.js
+++ scripts/index.js
@@ -1,37 +1,3 @@
-//загрузка начальных карточек
-const initialCards = [
-  {
-    name: 'Краснодар',
-    link: 'https://images.unsplash.com/photo-1563221923-d90d0a7dcf89?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1534&q=80',
-    alt: 'Цветущие поля с подсолнухами в Краснодарской области'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
-    alt: 'Фотография из Челябинской области'
-  },
-  {
-    name: 'Москва',
-    link: 'https://images.unsplash.com/photo-1541447271487-09612b3f49f7?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1534&q=80',
-    alt: 'Фотография Москва-Сити'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
-    alt: 'Фотография с Камчатки'
-  },
-  {
-    name: 'Карелия',
-    link: 'https://images.unsplash.com/photo-1602162993087-cbc22b61816e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1534&q=80',
-    alt: 'Фотография из Карелии'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
-    alt: 'Фотография Байкала'
-  }
-]; 
-
 const galleryTemplate = document.querySelector('.elements__template').content;
 const elements = document.querySelector('.elements');
 const popupPhoto = document.querySelector('.popup_id_photo');
